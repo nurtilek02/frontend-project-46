da0b37d9f9ba116d2fc5e09e3e0a82ca4864e38a
nurtilek02 <nurtilek.azhybekov@mail.ru>
2023-04-20 11:05:51 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,8 +2,16 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .name('gendiff.js')
-  .description('CLI to some JavaScript string utilities')
-  .version('0.0.1');
+   .name('gendiff')
+   .description('Compares two configuration files and shows a difference.')
+program
+   .option('-V, --version', 'output the version number')
+   .option('-f, --format <type>', 'output format')
+   .argument('<filepath1>')
+   .argument('<filepath2>')
+   .action((filepath1, filepath2) => {
+    console.log(filepath1, filepath2)
+  })   
+   .helpOption('-h, --help', 'output usage information')
 
   program.parse();
