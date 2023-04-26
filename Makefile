5ddfaf349795c0550a35a53562b1d91ac25e3667
nurtilek02 <nurtilek.azhybekov@mail.ru>
2023-04-26 10:10:04 +0300

    step six

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ install:
 	npm ci
 
 run:
-	node bin/gendiff.js ./__fixtures__/file1.json ./__fixtures__/file2.json
+	node bin/gendiff.js ./__fixtures__/file1.json ./__fixtures__/file2.json 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import { readFileSync } from 'node:fs';
-import path from 'node:path';
+import { cwd } from 'node:process';
 import _ from 'lodash';
+import path from 'node:path';
 
 export default function showDiff(filepath1, filepath2) {
-   const data1 = readFileSync(filepath1, { encoding: 'utf-8' })
-   const data2 = readFileSync(filepath2, { encoding: 'utf-8' })
+  const path1 = resolvePath(filepath1)
+  const path2 = resolvePath(filepath2)
+
+
+  const data1 = readFileSync(filepath1, { encoding: 'utf-8' })
+  const data2 = readFileSync(filepath2, { encoding: 'utf-8' })
    
-   const parsedObj1 = JSON.parse(data1)
-   const parsedObj2 = JSON.parse(data2)
+  const parsedObj1 = JSON.parse(data1)
+  const parsedObj2 = JSON.parse(data2)
    
   
-   generateDiff(parsedObj1, parsedObj2)
+  generateDiff(parsedObj1, parsedObj2)
 }
+
+function resolvePath(filepath) {
+  return filepath.includes('fixtures') 
+  ? filepath 
+  : `${path.resolve()}\\__fixtures__\\${filepath}`
+}
+
 
 function generateDiff (obj1, obj2) {
   const keys1 = Object.keys(obj1)
@@ -20,7 +32,7 @@ function generateDiff (obj1, obj2) {
   const sortedKeys = _.union(keys1, keys2).sort()
   
   const result = ['{'];
-  for (let key of sortedKeys) {
+    for (let key of sortedKeys) {
     if (Object.hasOwn(obj1, key)&&! Object.hasOwn(obj2, key)) {
     result.push(`  - ${key}: ${obj1[key]}`)
    } else if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
